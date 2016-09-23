1ebb41f85fc88e3ef3410ada392d6acc84808342
Devendra Gohel <lectroondev@gmail.com>
2016-09-23 12:18:27 +0530

    Examples list for object and collection with separation

--- ObjectInitilizerExample/Program.cs
+++ ObjectInitilizerExample/Program.cs
@@ -10,15 +10,26 @@ namespace ObjectInitilizerExample
     {
         static void Main(string[] args)
         {
-            List<Student> lstStudents = new List<Student>() {
-                new Student() { StudentId = 1, Name = "Sample 1", Age = 10 },
-                new Student() { StudentId = 2, Name = "Sample 2", Age = 20 },
-                };
+            ////Example 1: Object Initilizer
 
-            foreach (var item in lstStudents)
-            {
-                Console.WriteLine("Studnet Id :{0} \t Name :{1}",item.StudentId, item.Name);
-            }
+            Student student = new Student() { StudentId = 1, Name = "Sample 1", Age = 20 };
+            Console.WriteLine("Student Id:{0} \t Name: {1}", student.StudentId, student.Name);
+
+            //End Example 1
+
+            //Example 2: Collection Initilizer
+
+            //List<Student> lstStudents = new List<Student>() {
+            //    new Student() { StudentId = 1, Name = "Sample 1", Age = 10 },
+            //    new Student() { StudentId = 2, Name = "Sample 2", Age = 20 },
+            //    };
+
+            //foreach (var item in lstStudents)
+            //{
+            //    Console.WriteLine("Studnet Id :{0} \t Name :{1}",item.StudentId, item.Name);
+            //}
+
+            //End Example 2
 
 
 
